6c4908e11dec908c079f30a7e08e5272ea83ad05
Maxim <mailtime2010@gmail.com>
2020-08-12 18:11:45 +0300

    covid mouse drag/click

--- src/main/java/com/covid/Launch.java
+++ src/main/java/com/covid/Launch.java
@@ -5,9 +5,12 @@ import com.covid.datafetch.DataProviderService;
 import com.covid.datafetch.model.DataModel;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
@@ -19,14 +22,37 @@ import java.io.IOException;
  * @since 8/12/2020
  */
 public class Launch extends Application {
+    private double xOffset;
+    private double yOffset;
+
     @Override
     public void start(final Stage primaryStage) throws IOException {
-        final Parent root = FXMLLoader.load(getClass().getResource("/widget.fxml"));
-
-        final Scene scene = new Scene(root);
-        primaryStage.setScene(scene);
+        primaryStage.initStyle(StageStyle.UTILITY);
+        primaryStage.setOpacity(0);
         primaryStage.show();
+
+        final Stage secondaryStage = new Stage();
+        secondaryStage.initStyle(StageStyle.UNDECORATED);
+        secondaryStage.initOwner(primaryStage);
+
+        final Parent root = FXMLLoader.load(getClass().getResource("/widget.fxml"));
+        final Scene scene = new Scene(root);
+        secondaryStage.setScene(scene);
+        secondaryStage.show();
         final DataModel model = new DataProviderService().getData("Germany");
         System.out.println(model);
+        final Rectangle2D visualBounds = Screen.getPrimary().getVisualBounds();
+        secondaryStage.setX(visualBounds.getMaxX() - 25 - scene.getWidth());
+        secondaryStage.setY(visualBounds.getMinY() + 25);
+
+        scene.setOnMousePressed(event -> {
+            this.xOffset = secondaryStage.getX() - event.getScreenX();
+            this.yOffset = secondaryStage.getY() - event.getScreenY();
+        });
+        scene.setOnMouseDragged(event -> {
+            secondaryStage.setX(event.getScreenX() + this.xOffset);
+            secondaryStage.setY(event.getScreenY() + this.yOffset);
+        });
+
     }
 }
